e3ac0b194a651456f2e4e2df451e53615e45fa4e
Rafael Fantini da Costa <rafael.costa@mnix.com.br>
2014-01-15 03:07:46 -0200

    Using T4 to automate bootstrap generation.

--- Rest.Rest/Mnix.Plugins.Rest.Droid/RestPluginBootstrap.cs
+++ Rest.Rest/Mnix.Plugins.Rest.Droid/RestPluginBootstrap.cs
@@ -1,20 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Cirrious.CrossCore.Plugins;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Cirrious.CrossCore.Plugins;
-using Cirrious.CrossCore;
-
-namespace $myProject$.Droid.Bootstrap
+namespace Mnix.Plugins.Rest.Droid
 {
-    public class RestPluginBootstrap : MvxPluginBootstrapAction<Mnix.Plugins.Rest.RestPluginLoader>
+    public class RestPluginBootstrap
+        : MvxPluginBootstrapAction<Mnix.Plugins.Rest.RestPluginLoader>
     {
     }
 }
